5b05e4fa68125920f853590b6ff156733d4e3b4f
Tshegofatso <u18189424@tuks,co.za>
2020-09-24 16:47:14 +0200

    Updated About page

--- ThirdEye-Pictures/src/views/About.vue
+++ ThirdEye-Pictures/src/views/About.vue
@@ -3,13 +3,15 @@
     <div class="hero is-primary">
       <div class="hero-body">
         <div class="container">
-          <h1 class="title is-size-1">About Animal Rescue League</h1>          
+          <h1 class="title is-size-1">About ThirdEye Pictures</h1>          
         </div>
       </div>
     </div>
     <div class="container">
-      <p class="org-description is-size-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quia aperiam eligendi dolorum reprehenderit ea amet, aliquid dolorem beatae, iste aliquam ullam. Sequi ab eligendi consectetur neque laudantium, libero asperiores.</p>
-      <p class="org-description is-size-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quia aperiam eligendi dolorum reprehenderit ea amet, aliquid dolorem beatae, iste aliquam ullam. Sequi ab eligendi consectetur neque laudantium, libero asperiores.</p>
+      <p class="org-description is-size-4">ThirdEye pictures is a content creation company. With the collaborative efforts of a talented team, we excel in branding, developing engaging content and launching custom strategic campaigns through digital media outlets.</p>
+      <br/>
+      <h2 class="title is-size-3">Why Work With Us?</h2>   
+      <p class="org-description is-size-4">We make big things happen and we always to do better. We understand the science behind content creationa dngive you the best value for your money.</p>
     </div>
   </div>
 </template>
